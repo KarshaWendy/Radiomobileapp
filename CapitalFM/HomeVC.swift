3b509456fcd4ad51d530bcc1fb03f49a84dbad3b
Fred Oywer <fredrick.oywer@smartapplicationsgroup.com>
2019-09-13 12:18:45 +0300

    Ïnitial

---
+++ CapitalFM/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  CapitalFM
+//
+//  Created by mac on 13/09/2019.
+//  Copyright © 2019 Smart Applications. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
