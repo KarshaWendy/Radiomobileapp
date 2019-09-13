bff1ebd88053300015d805573a03b6688640e575
Fred Oywer <fredrick.oywer@smartapplicationsgroup.com>
2019-09-13 09:26:14 +0300

    Initial Commit

---
+++ CapitalFM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapitalFM
+//
+//  Created by mac on 13/09/2019.
+//  Copyright © 2019 Smart Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
