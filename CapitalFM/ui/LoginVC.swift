16c7d3a113df48f35036116b8d8fc5c19c9e1e17
Fred Oywer <fredrick.oywer@smartapplicationsgroup.com>
2019-10-08 17:11:39 +0300

    8th Oct 19

--- CapitalFM/ui/LoginVC.swift
+++ CapitalFM/ui/LoginVC.swift
@@ -7,36 +7,74 @@
 //
 
 import UIKit
-import GoogleSignIn
+import MBProgressHUD
+import Alamofire
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var ivBg: UIImageView!
+    @IBOutlet weak var viewLogin: UIView!
+    @IBOutlet weak var ViewRegister: UIView!
+    @IBOutlet weak var btnSignUp: UIButton!
     @IBOutlet weak var btnSignIn: UIButton!
-    @IBOutlet weak var viewFB: UIView!
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPass: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
     
-    override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.navigationBar.isHidden = false
-    }
+    var appUtil = AppUtil()
+    var email = ""
+    var password = ""
     
+    //    override func viewWillAppear(_ animated: Bool) {
+//        self.navigationController?.navigationBar.isHidden = false
+//    }
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.title = "Login"
-        
-        btnSignIn.backgroundColor = UIColor.MyTheme.primaryColor
-        
-        viewFB.backgroundColor = UIColor.MyTheme.fbColor
+        btnLogin.backgroundColor = UIColor.MyTheme.primaryColor
+        btnSignIn.setTitleColor(UIColor.MyTheme.primaryColor, for: .normal)
+        ViewRegister.isHidden = true
+//        ivBg.tintColor = .darkGray
+   
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnSignIn(_ sender: Any) {
+        ViewRegister.isHidden = true
+        viewLogin.isHidden = false
+        btnSignIn.setTitleColor(UIColor.MyTheme.primaryColor, for: .normal)
+        btnSignUp.setTitleColor(UIColor.white, for: .normal)
+        
     }
-    */
+    
+    @IBAction func btnSignUp(_ sender: Any) {
+        ViewRegister.isHidden = false
+        viewLogin.isHidden = true
+        btnSignIn.setTitleColor(UIColor.white, for: .normal)
+        btnSignUp.setTitleColor(UIColor.MyTheme.primaryColor, for: .normal)
+    }
+    
+    @IBAction func btnLogin(_ sender: Any) {
+        email = tfEmail.text!
+        password = tfPass.text!
+        
+        if !email.isEmpty && !password.isEmpty {
+            if appUtil.isValidEmail(enteredEmail: email){
+                login()
+            } else {
+                appUtil.showAlert(title: "", msg: "Enter a valid email address")
+            }
+        } else {
+            appUtil.showAlert(title: "", msg: "Fill in both fields")
+        }
+    }
+    
+    func login(){
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        let params = ["": ""]
+        let headers = ["": ""]
+        Alamofire.request(MyConstants().SOUNDCLOUD_CLIENT_ID, method: .post, parameters: params, encoding: nil, headers: headers)
+    }
 
 }
